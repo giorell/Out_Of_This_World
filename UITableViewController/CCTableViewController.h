3112952803efa79d4824452ebab9750a33c1c775
Giordany Orellana <giorell@gmail.com>
2015-07-26 18:17:03 -0700

    solved the challenge

--- UITableViewController/CCTableViewController.h
+++ UITableViewController/CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CCTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Giordany Orellana on 7/26/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CCTableViewController : UITableViewController
 
 @end
-
