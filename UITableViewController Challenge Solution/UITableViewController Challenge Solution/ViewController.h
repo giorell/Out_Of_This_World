e5bf897046892b2ef6dde5bac18daf9a588438b3
Giordany Orellana <giorell@gmail.com>
2015-07-26 17:46:08 -0700

    Created Project

---
+++ UITableViewController Challenge Solution/UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Giordany Orellana on 7/26/15.
+//  Copyright (c) 2015 Giordany Orellana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewController/UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Giordany Orellana on 7/26/15.
+//  Copyright (c) 2015 Giordany Orellana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
